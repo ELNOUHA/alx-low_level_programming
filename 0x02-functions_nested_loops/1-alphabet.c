c0c633cd8fbf5a8fd9d49a222f9f30cc29fb829e
Nouhaila <elmlasnohaila@gmail.com>
2023-06-20 11:26:32 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Descrption: print the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n')
+
+		return (0);
+}
