1005d20d4ab1eeebe305618f31300c14b2d951cf
Nouhaila <elmlasnohaila@gmail.com>
2023-06-22 09:10:47 -0700

    num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ *            only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar (i + 48);
+		_putchar('\n');
+	}
+}
