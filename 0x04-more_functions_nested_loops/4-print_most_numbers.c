0b08672c8715b4a2e22c9d52d614c828ff69817a
Nouhaila <elmlasnohaila@gmail.com>
2023-06-22 09:52:21 -0700

    numZ

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,12 +11,10 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 48; num <= 58; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		if ((num == 50) || (num == 52))
-		{
+		if ((num == 2) || (num == 4))
 			continue;
-		}
 		_putchar(num + 48);
 	}
 	_putchar('\n');
