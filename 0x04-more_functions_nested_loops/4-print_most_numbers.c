53cc192e598623693c9befa55020131b5dc889a8
Nouhaila <elmlasnohaila@gmail.com>
2023-06-22 09:37:32 -0700

    num012

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,9 @@
-
 #include "main.h"
 
 /**
  * print_most_numbers - prints the numbers from 0 to 9
  *                 do not print 2 and 4
  *                 only using _putchar twice
- *
  * Return: Always 0 (Success)
 */
 
@@ -13,9 +11,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == 2 || i == 4)
+		if (i == '2' || i == '4')
 			continue;
 		_putchar(i + 48);
 	}
