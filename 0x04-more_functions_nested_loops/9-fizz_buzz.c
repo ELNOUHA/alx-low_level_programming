7a824b73567e03d7530d26015c8198dcc72fff1e
Nouhaila <elmlasnohaila@gmail.com>
2023-06-22 16:52:07 -0700

    mlp

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints either number
+ * or fizz or buzz or fizzBuzz
+ *
+ *
+ * Return: returns 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && !(num % 5 == 0))
+			printf("Fizz");
+		else if if ((num % 5 == 0) && !(num % 3 == 0))
+			printf("Buzz");
+		else if ((num % 3 == 0) && (num % 5 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d ", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
